6e9b378a8870bef140c7c00494748b59c5b05f52
bastianhenneberg <69174294+bastianhenneberg@users.noreply.github.com>
2023-09-06 01:35:43 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,26 +10,20 @@
 &kp ESC,           &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp LBKT,          &kp COMMA,         &kp DOT,           &kp RBKT,         \
 U_NP,              U_NP,              &kp LCTRL,         &kp SPC,           &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
-#define MIRYOKU_LAYER_DONE \
-&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp TILDE,         U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-&kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp F5,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-&kp Q,             &kp W,             &kp C,             &kp I,             &kp T,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-U_NP,              U_NP,              U_LT(U_DTWO, ESC), &kp SPC,           &kp LSHFT,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-
-#define MIRYOKU_LAYER_DTWO \
-&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp TILDE,         U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-&kp F6,            &kp F7,            &kp F8,            &kp F9,            &kp F10,           U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-&kp P,             &kp M,             &kp S,             &kp I,             &kp T,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
-U_NP,              U_NP,              U_NA,              &kp SPC,           &kp LSHFT,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-
-
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       &to U_GAME K05  K06  K07  K08  K09  XXX \
+XXX  K00  K01  K02  K03  K04       &lt U_GAME K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                  K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       &to U_DONE K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       &to U_BASE K35  K36  K37
+XXX  K20  K21  K22  K23  K24                  K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34                  K35  K36  K37
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(GAME,   "Gaming") \
+
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+
+#define U_GAME   10
