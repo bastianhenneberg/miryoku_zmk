0a662772cc4e4af154ac3e99ef537cb42d5115de
bastianhenneberg <69174294+bastianhenneberg@users.noreply.github.com>
2023-09-06 23:07:08 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,7 +10,7 @@
 
 // FPS friendly tap layer
 #define MIRYOKU_LAYER_GERMAN \
-&kp TAB,           &kp Q,             &kp CAPS,          &kp E,             &euros,           &kp T,             &kp NUM_1,         &kp NUM_2,         &kp NUM_3,       &kp NUM_3,    \
+&kp TAB,           &kp Q,             &kp CAPS,          &kp E,             &kp euros,         &kp T,             &kp NUM_1,         &kp NUM_2,         &kp NUM_3,       &kp NUM_3,    \
 &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             U_MS_L,            U_MS_D,            U_MS_U,          U_MS_U,       \
 &kp ESC,           &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp LBKT,          &kp COMMA,         &kp DOT,         &kp DOT,      \
 U_NP,              U_NP,              &kp LCTRL,         &kp SPC,           &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,             
