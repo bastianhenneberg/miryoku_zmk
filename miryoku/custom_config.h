ce9da569033929a5cd9165aa934ab104c6802dec
bastianhenneberg <69174294+bastianhenneberg@users.noreply.github.com>
2023-09-06 02:10:32 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -29,10 +29,10 @@ U_NP,              U_NP,              U_NA,              &kp SPC,           &kp 
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       &lt U_Game K05  K06  K07  K08  K09  XXX \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       &lt U_DONE K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       &lt U_BASE K35  K36  K37
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
