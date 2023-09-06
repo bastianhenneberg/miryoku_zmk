5676342172b707cf2631dd5b35c95b3af1afeb3d
bastianhenneberg <69174294+bastianhenneberg@users.noreply.github.com>
2023-09-06 02:05:46 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -23,16 +23,16 @@ U_NP,              U_NP,              U_LT(U_DTWO, ESC), &kp SPC,           &kp 
 U_NP,              U_NP,              U_NA,              &kp SPC,           &kp LSHFT,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 // Direct custom layer access
-#define MIRYOKU_LAYOUTMAPPING_CORNE(\
-K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-K00  K01  K02  K03  K04  XXX  &to U_GAME K05  K06  K07  K08  K09 \
-K10  K11  K12  K13  K14  XXX  XXX        K15  K16  K17  K18  K19 \
-K20  K21  K22  K23  K24  XXX  &to U_DONE K25  K26  K27  K28  K29 \
-          K32  K33  K34  XXX  &to U_BASE K35  K36  K37
+XXX  K00  K01  K02  K03  K04       &lt U_Game K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       &lt U_DONE K25  K26  K27  K28  K29  XXX \
+               K32  K33  K34       &lt U_BASE K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
